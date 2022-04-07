d71087869cf8a11ce98d4d674da98f29e6fc76ae
Taemin Im <devTaemin@gmail.com>
2022-04-07 23:09:37 +0900

    Update UnmakablePrice.java

--- Greedy/UnmakablePrice.java
+++ Greedy/UnmakablePrice.java
@@ -1,66 +1,35 @@
 public class UnmakablePrice {
 	public static int N;
-	public static int[] array;
-	public static boolean[] visited;
-	public static boolean[] result;
-	
-	
-	public static void Combination(int start, int depth, int size) {
-		
-		if (depth == size) {
-			int total = 0;
-			for (int i = 0; i < array.length; i++) {
-				if (visited[i] == true) {
-					//System.out.printf("%d ", array[i]);
-					total += array[i];
-				}
-			}
-			//System.out.println();
-			result[total] = true;
-			return;
-		}
-		
-		for (int i = start; i < array.length; i++) {
-			if (visited[i] == false) {
-				visited[i] = true;
-				Combination(i, depth + 1, size);
-				visited[i] = false;
-			}
-		}
-		
-		
-	}
-	
+	public static ArrayList<Integer> array = new ArrayList<Integer>();
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
-		array = new int[N];
-		visited = new boolean[N];
 		
-		int total = 0;
 		for (int i = 0; i < N; i++) {
-			array[i] = sc.nextInt();
-			total += array[i];
+			array.add(sc.nextInt());
 		}
 		
-		// From 1 to total + 1
-		result = new boolean[total + 1]; 
+		Collections.sort(array);
+		Iterator<Integer> iter = array.iterator();
 		
-		// Sort in Ascending order
-		Arrays.sort(array);
+		// Target number to make
+		int target = 1;
 		
-		for (int i = 1; i <= N; i++) {
-			Combination(0, 0, i);
+		while(iter.hasNext()) {
+			// Get coin from array
+			int coin = iter.next();
+			
+			// Assume that the price below target - 1 (include) is possible
+			// If the coin is beyond target, target is not possible 
+			if (target < coin) break;
+			
+			// Next target is (target + coin)
+			// Because (target - 1) is possible, then the price until target + coin - 1 is possible
+			target += coin;
 		}
 		
-		for (int i = 1; i <= total; i++) {
-			if (result[i] == false) {
-				System.out.println(i);
-				break;
-			}
-		}
+		System.out.println(target);
 		
 	}
-
 }
