2c044828ee826447b4c0c27af57b10ca75805242
Taemin Im <devTaemin@gmail.com>
2022-04-07 16:43:54 +0900

    Create UntilOne.java

---
+++ Greedy/UntilOne.java
@@ -0,0 +1,40 @@
+public class UntilOne {
+	public static int N, K;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		K = sc.nextInt();
+		
+//		int count = 0;
+//		while(N > 1) {
+//			N = (N % K == 0) ? N / K : N - 1;			
+//			count++;
+//		}
+		
+		// If The number of N is over 100_000_000_000 ?
+		int count = 0;
+		int result = 0;
+		int rest = 0;
+		while (true) {
+			result = N / K;
+			rest = N % K;
+			
+			// First, Subtract one until N could be divided by K
+			// Second, Add one for division
+			count += (rest + 1);
+			
+			// Third, Check out the result of division, if it is lower than K
+			if (result < K) {
+				count += (result - 1);
+				break;
+			}
+			
+			// Fourth, N refers to the result of division
+			N = result;
+		}
+
+		System.out.println(count);
+	}
+
+}
